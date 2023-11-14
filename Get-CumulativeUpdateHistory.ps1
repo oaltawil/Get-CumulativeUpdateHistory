e7431e3a32ba558f1346a6c64effa895995f9193
Osama Altawil <no-reply>
2023-11-14 17:41:57 -0500

    Minor edits to help messages.

--- Get-CumulativeUpdateHistory.ps1
+++ Get-CumulativeUpdateHistory.ps1
@@ -13,7 +13,7 @@ This script compares the release dates of the installed cumulative update and th
 3. Returns the number of days that have passed from the release date of the installed update to the release date of the latest available update. The return type is a JSON object with a single (integer) property called NumberOfDaysSinceLCU.
 
 Notes:
-- If the OS Build of the running operating system cannot be found in the Windows Update History webpage, then no cumulative updates have been installed. Non-cumulative updates, typically necessary servicing stack updates, were most probably included in the image, or were installed by Windows Setup Dynamic Update or by Windows Update client from Microsoft Update/WSUS/MECM. In this case, the script uses the release date of the initial build of the Windows 10/11 feature update or version, e.g. October 04, 2021 for Windows 11 21H2.
+- If the OS Build of the running operating system cannot be found in the Windows Update History webpage, then no cumulative updates have been installed. Non-cumulative updates, typically necessary servicing stack updates or updates to the Windows Update client, were most probably included in the image, or were installed by Windows Setup Dynamic Update or by Windows Update client from Microsoft Update/WSUS/MECM. In this case, the script uses the release date of the initial build of the Windows 10/11 feature update or version, e.g. October 04, 2021 for Windows 11 21H2.
 - If the latest cumulative update cannot be found in the Windows Update History webpage, then the script returns zero for the 'NumberOfDaysSinceLCU' property. This can happen for a freshly released Windows 11 feature update or version, e.g. 23H2, that doesn't have any non-Preview cumulative updates published yet (as of November 10, 2023).
 
 Tip: Use the -Verbose common parameter to display details about the installed and latest cumulative updates.
@@ -199,9 +199,8 @@ if ($InstalledUpdateLink) {
 
 }
 else {
-# If the OS Build cannot be found in the Windows Update History webpage, then no cumulative updates have been installed. Non-cumulative updates were included in the image, or were installed by Windows Setup Dynamic Updates or by Windows Update client from Microsoft Update/WSUS/MECM
-    # Return the release date of the initial build of the Windows 11/10 feature update or version
-    # Create an object with a single property ReleaseDate, which is the initial release date of the Windows 11/10 Feature Update or Version.
+    # If the OS Build cannot be found in the Windows Update History webpage, then no cumulative updates have been installed.
+    # Return an object with a single property ReleaseDate, which is the initial release date of the Windows 11/10 Feature Update or Version.
     $InstalledUpdateInfo = [PSCustomObject]@{ReleaseDate = [datetime]$WindowsUpdateHistory.InitialReleaseDate}
 }
 
